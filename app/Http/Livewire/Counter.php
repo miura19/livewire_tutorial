d42ae3469bf9d992efb56734b56244248527d4a7
miura19 <littlecloud194@gmail.com>
2022-05-02 22:31:47 +0900

    ★一番上と一番下にdivタグを書いてその中にごにょごにょ書かないとwire:click動かん★

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 10;
+
+    public function inc(){
+        $this->count++;
+    }
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>{{-- 一番上にdivタグ--}}
+    <div>
+        <h1>初めてのLivewire!!</h1>
+    </div>
+    <div>
+        <h2>{{ $count }}</h2>
+    </div>
+    <p><button wire:click="inc">+1</button></p>
+</div>{{-- 一番下にdivタグやんないとwire:clicl動かん--}}
